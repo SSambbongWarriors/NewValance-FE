06259e8269db4c1796537d193abae32182f1223a
m-oooooonchild <syp614@ewhain.net>
2025-02-18 03:41:36 +0900

    💄 비디오 재생/정지 및 아이콘 추가

--- NewValance/src/components/VideoPage/VideoPlayer.tsx
+++ NewValance/src/components/VideoPage/VideoPlayer.tsx
@@ -1,6 +1,11 @@
-import { useEvent } from 'expo';
 import { useVideoPlayer, VideoView } from 'expo-video';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as S from './VideoPlayer.styles';
+
+import { Pressable } from 'react-native';
+import { CustomText } from '../common/CustomText';
+import theme from '../../styles/theme';
+import { AnimatedIcon } from './AnimatedIcon';
 
 interface VideoPlayerProps {
   src: string;
@@ -8,30 +13,72 @@ interface VideoPlayerProps {
 }
 
 export const VideoPlayer = ({ src, isPlaying }: VideoPlayerProps) => {
-  const [isPaused, setIsPaused] = useState(isPlaying);
+  const [isPaused, setIsPaused] = useState(!isPlaying);
 
   const player = useVideoPlayer(src, (player) => {
     player.loop = true;
-    isPaused ? player.pause() : player.play();
   });
 
-  const handleVideoPress = () => {
+  useEffect(() => {
+    if (!isPlaying) {
+      player.pause();
+    } else {
+      player.replay();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
     if (isPaused) {
       player.pause();
     } else {
       player.play();
     }
-    setIsPaused(!isPaused);
+  }, [isPaused]);
+
+  const handleVideoPress = () => {
+    setIsPaused((prev) => !prev);
+    console.log('press');
   };
 
   return (
-    <VideoView
-      player={player}
-      onTouchEnd={handleVideoPress}
-      allowsPictureInPicture
-      contentFit="contain"
-      nativeControls={false}
-      style={{ flex: 1 }}
-    />
+    <S.Container>
+      <VideoView
+        player={player}
+        contentFit="contain"
+        nativeControls={false}
+        style={{
+          flex: 1,
+          width: '100%',
+          height: '100%',
+        }}
+      />
+      <S.PressableArea onPress={handleVideoPress} />
+      <AnimatedIcon isPaused={isPaused} />
+      <S.LeftArrow onPress={() => console.log('press')} />
+      <S.Menu />
+      <S.IconContainer>
+        <S.Heart />
+        <S.Chat />
+        <S.Share />
+      </S.IconContainer>
+      <S.InfoContainer>
+        <S.Title>
+          <CustomText
+            font={theme.fonts.bold18}
+            color={theme.colors.white}
+            numberOfLines={1}
+            ellipsizeMode="tail"
+          >
+            설 연휴 마지막 날 '한파'...아침 최저 -10도 안팎 강추위
+          </CustomText>
+        </S.Title>
+        <S.LinkButton onPress={() => console.log('press')}>
+          <CustomText font={theme.fonts.bold14} color={theme.colors.gray_4}>
+            기사 원문
+          </CustomText>
+          <S.Arrow />
+        </S.LinkButton>
+      </S.InfoContainer>
+    </S.Container>
   );
 };
--- NewValance/src/pages/Video/VideoPage.tsx
+++ NewValance/src/pages/Video/VideoPage.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { VideoPlayer } from '../../components/VideoPage/VideoPlayer';
 import * as S from './VideoPage.styles';
-import { View } from 'react-native';
-import { Text } from 'react-native-svg';
 
 interface VideoData {
   id: number;
@@ -11,16 +9,12 @@ interface VideoData {
 
 const dummyData = [
   {
-    id: 0,
-    src: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
-  },
-  {
     id: 1,
-    src: 'https://sample-videos.com/video123/mp4/720/elephants-dream_720p_10mb.mp4',
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
   },
   {
     id: 2,
-    src: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
   },
   {
     id: 3,
@@ -32,49 +26,45 @@ const dummyData = [
   },
   {
     id: 5,
-    src: 'https://mazwai.com/videvo_files/video/free/2015-10/small_waterfall.mp4',
+    src: 'https://filesamples.com/samples/video/mp4/sample_960x400_ocean_with_audio.mp4',
   },
   {
     id: 6,
-    src: 'https://mazwai.com/videvo_files/video/free/2016-02/large_preview_0556_cloud_timelapse.mp4',
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
   },
   {
     id: 7,
-    src: 'https://filesamples.com/samples/video/mp4/sample_960x400_ocean_with_audio.mp4',
-  },
-  { id: 8, src: 'https://dl8.webmfiles.org/big-buck-bunny_trailer.mp4' },
-  {
-    id: 9,
-    src: 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
   },
 ];
 
 const VideoPage = () => {
+  const [videos, setVideos] = useState<VideoData[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const shouldRenderVideo = (index: number) => {
-    // 현재 인덱스 기준으로 ±2 인덱스만 비디오를 렌더링
-    return Math.abs(currentIndex - index) <= 2;
-  };
+  useEffect(() => {
+    setVideos(dummyData);
+  }, []);
 
   return (
     <S.Container>
+      {/* <VideoPlayer src={dummyData[0].src} isPlaying={true} /> */}
       <S.VideoList
-        data={dummyData}
-        keyExtractor={(item: any) => item.id}
-        renderItem={({ item, index }: any) =>
-          // 현재 보이는 페이지와 인접한 페이지만 비디오를 렌더링
-          shouldRenderVideo(index) ? (
-            <VideoPlayer src={item.src} isPlaying={currentIndex === index} />
-          ) : (
-            <Text>Video Placeholder</Text>
-          )
-        }
-        pagingEnabled
+        data={videos}
+        keyExtractor={(item: any) => item.id.toString()}
+        renderItem={({ item, index }: any) => (
+          <VideoPlayer src={item.src} isPlaying={index === currentIndex} />
+        )}
+        pagingEnabled={true}
         horizontal
         showsHorizontalScrollIndicator={false}
-        initialNumToRender={3}
-        windowSize={5}
+        onMomentumScrollEnd={(event: any) => {
+          const newIndex = Math.round(
+            event.nativeEvent.contentOffset.x /
+              event.nativeEvent.layoutMeasurement.width
+          );
+          setCurrentIndex(newIndex);
+        }}
       />
     </S.Container>
   );
