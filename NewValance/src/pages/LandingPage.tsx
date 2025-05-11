aeb7ada9f61b334dc13ace266ba2b2f8e6afcd41
seoyonara <syp614@ewhain.net>
2025-05-11 19:52:44 +0900

    ✨ 토큰 유효성 검증

--- NewValance/src/pages/LandingPage.tsx
+++ NewValance/src/pages/LandingPage.tsx
@@ -2,14 +2,57 @@ import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { useEffect } from 'react';
 import { Image, ImageBackground, View } from 'react-native';
 import SplashLogo from '../assets/images/common/splash-icon.svg';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const LandingPage = () => {
-  const navigation = useNavigation<NavigationProp<any>>();
+  const navigation = useNavigation<StackNavigationProp<any>>();
+
+  const parseJwt = (token: string) => {
+    try {
+      const base64Payload = token.split('.')[1];
+      const decodedPayload = atob(base64Payload);
+      return JSON.parse(decodedPayload);
+    } catch (error) {
+      console.error('JWT 디코딩 실패:', error);
+      return null;
+    }
+  };
+
+  const isTokenValid = (accessToken: string): boolean => {
+    const payload = parseJwt(accessToken);
+    if (!payload?.exp) return false;
+    return payload.exp * 1000 > Date.now(); // 현재 시간보다 exp가 미래인지 확인
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      navigation.navigate('Login');
-    }, 1000);
+    const checkToken = async () => {
+      try {
+        const rawToken = await AsyncStorage.getItem('token');
+        if (!rawToken) {
+          setTimeout(() => {
+            navigation.replace('Login');
+          }, 1000);
+        }
+
+        const token = JSON.parse(rawToken as string);
+        const accessToken = token?.accessToken;
+
+        if (accessToken && isTokenValid(accessToken)) {
+          setTimeout(() => {
+            navigation.replace('Home');
+          }, 1000);
+        } else {
+          setTimeout(() => {
+            navigation.replace('Login');
+          }, 1000);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    checkToken();
   }, []);
 
   return (
